4e634eac30f5b922eb18d5958a85f49918171232
FarzamAlam <farzamcse@gmail.com>
2020-06-20 13:57:50 +0530

    Added a mongodb client

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/go-redis/redis/v8 v8.0.0-beta.5
 	github.com/gorilla/mux v1.7.4
+	go.mongodb.org/mongo-driver v1.3.4
 )
--- handlers/indexHandler.go
+++ handlers/indexHandler.go
@@ -7,6 +7,9 @@ import (
 	"net/http"
 
 	"github.com/go-redis/redis/v8"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // Creating a template instance so that we can execute our data into it.
@@ -27,5 +30,16 @@ func init() {
 			DB:       0,
 		})
 	pong, _ := rdb.Ping(ctx).Result()
-	fmt.Println(pong)
+	fmt.Println("Redis Ping : ", pong)
+
+	// MongoDB.
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		fmt.Println("Error while Connecting to mongodb : ", err)
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	fmt.Println("MongoDb err : ", err)
+	collection := client.Database("temp").Collection("users")
+	res := collection.FindOne(ctx, nil)
+	fmt.Println(res)
 }
