18f02a7862b12691d9daf16e0c9d8f9e4f881566
FarzamAlam <farzamcse@gmail.com>
2020-06-21 15:58:46 +0530

    Added SetCacheURL and GetCacheURL

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module short-url
 go 1.13
 
 require (
+	github.com/go-redis/redis v6.15.8+incompatible
 	github.com/go-redis/redis/v8 v8.0.0-beta.5
 	github.com/gorilla/mux v1.7.4
 	github.com/joho/godotenv v1.3.0
--- models/cache.go
+++ models/cache.go
@@ -1,1 +1,46 @@
 package models
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/joho/godotenv"
+)
+
+var rdb *redis.Client
+
+func init() {
+	_ = godotenv.Load()
+	host := os.Getenv("cacheDB_host")
+	port := os.Getenv("cacheDB_port")
+	uri := fmt.Sprintf("%s:%s", host, port)
+
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     uri,
+		DB:       0,
+		Password: "",
+	})
+	pong, _ := rdb.Ping(ctx).Result()
+	fmt.Println("Ping - ", pong)
+	url := &URL{
+		ShortURL:    "1e4c36",
+		OriginalURL: "https://github.com/farzamalam",
+	}
+	_ = url.SetCacheURL()
+	u, _ := url.GetCacheURL()
+	fmt.Println("u : ", u)
+}
+
+func GetRedisClient() *redis.Client {
+	return rdb
+}
+
+func (url *URL) SetCacheURL() error {
+	return GetRedisClient().Set(ctx, url.ShortURL, url.OriginalURL, time.Hour*2).Err()
+}
+
+func (url *URL) GetCacheURL() (string, error) {
+	return GetRedisClient().Get(ctx, url.ShortURL).Result()
+}
